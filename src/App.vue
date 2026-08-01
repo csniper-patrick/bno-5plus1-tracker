feee5785b9c367a0894c59f8232383078616e607
Patrick Pun <patrickwypun04@gmail.com>
2026-08-01 14:34:38 +0000

    refactor: center app layout content with a constrained max-width container and remove global reset styles

--- src/App.vue
+++ src/App.vue
@@ -13,34 +13,34 @@ function toggleTheme() {
 <template>
   <v-app>
     <v-app-bar color="primary" elevation="2" class="px-2 px-sm-4">
-      <template #prepend>
+      <div class="w-100 mx-auto d-flex align-center" style="max-width: 1600px;">
         <v-icon icon="mdi-passport" size="large" class="ml-2 ml-sm-3 mr-2"></v-icon>
-      </template>
-      <v-app-bar-title class="font-weight-bold text-truncate flex-shrink-1">
-        BNO 5+1 Tracker
-      </v-app-bar-title>
+        <v-app-bar-title class="font-weight-bold text-truncate flex-shrink-1">
+          BNO 5+1 Tracker
+        </v-app-bar-title>
 
-      <v-spacer class="d-none d-sm-flex"></v-spacer>
+        <v-spacer class="d-none d-sm-flex"></v-spacer>
 
-      <v-chip
-        size="small"
-        color="secondary"
-        class="d-none d-sm-inline-flex mr-2 font-weight-bold"
-        variant="flat"
-        prepend-icon="mdi-crown-outline"
-      >
-        UK BNO Path
-      </v-chip>
+        <v-chip
+          size="small"
+          color="secondary"
+          class="d-none d-sm-inline-flex mr-2 font-weight-bold"
+          variant="flat"
+          prepend-icon="mdi-crown-outline"
+        >
+          UK BNO Path
+        </v-chip>
 
-      <v-btn
-        :icon="theme.global.current.value.dark ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-        variant="text"
-        @click="toggleTheme"
-      ></v-btn>
+        <v-btn
+          :icon="theme.global.current.value.dark ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+          variant="text"
+          @click="toggleTheme"
+        ></v-btn>
+      </div>
     </v-app-bar>
 
     <v-main>
-      <v-container max-width="1600" class="py-6 px-4 px-md-8">
+      <v-container class="mx-auto py-6 px-4 px-md-8" style="max-width: 1600px;">
         <RouterView />
       </v-container>
     </v-main>
